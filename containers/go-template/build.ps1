c9710b6ff902917fbde6c0c129d593841020f5bd
Mickaël Rémond <mremond@process-one.net>
2017-03-07 22:35:40 +0100

    Generate self-signed certificate to start HTTP/2 server

--- containers/go-template/build.ps1
+++ containers/go-template/build.ps1
@@ -3,7 +3,14 @@ mkdir build
 # On Windows, we use Go Docker build container to compile a Go app for Linux
 # TODO Dockerfile.build
 
+# Generate self-signed cert
+$env:GOBIN="$pwd" + "/build/"
+go install $env:GOROOT/src/crypto/tls/generate_cert.go
+build/generate_cert --host localhost
+mv cert.pem build/
+mv key.pem build/
+
 # Create minimal container with the result of the build
-go build -o build/server server/server.go
+go build -o build/server.exe server/server.go
 
 docker build -t fluux-go-tmpl -f Dockerfile .
--- containers/go-template/server/server.go
+++ containers/go-template/server/server.go
@@ -1,7 +1,31 @@
 package main
 
-func main() {
+import (
+	"io"
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/pushed", pushed)
+
+	http.HandleFunc("/", rootHandler)
+
+	http.ListenAndServeTLS(":443", "cert.pem", "key.pem", nil)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	if pusher, ok := w.(http.Pusher); ok {
+		if err := pusher.Push("/pushed", nil); err != nil {
+			log.Println("push failed")
+		}
+	}
+
+	io.WriteString(w, "hello world")
+}
+
+func pushed(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "hello server push")
 }
 
 // TODO move the reusable part into a library
